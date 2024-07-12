44f98f2389585822cbf4f27640a860dab42854c5
avachoi <alexbchoi@gmail.com>
2024-07-12 13:02:04 -0400

    created package

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("hellow");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const morgan = require("morgan");
+
+const app = express();
+const port = 8080;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+	const time = new Date();
+
+	console.log(
+		`-----
+${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+	);
+	if (Object.keys(req.body).length > 0) {
+		console.log("Containing the data:");
+		console.log(`${JSON.stringify(req.body)}`);
+	}
+	next();
+});
+//using morgan
+app.use(
+	morgan(function (tokens, req, res) {
+		return [
+			tokens.method(req, res),
+			tokens.url(req, res),
+			tokens.status(req, res),
+			tokens.res(req, res, "content-length"),
+			"-",
+			tokens["response-time"](req, res),
+			"ms",
+		].join(" ");
+	})
+);
+app.get("/", (req, res) => {
+	throw new Error("BROKEN"); // Express will catch this on its own.
+});
+app.listen(port, () => {
+	console.log(`Server listening on port:${port}`);
+});

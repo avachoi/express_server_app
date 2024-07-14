5aa381d6ad25f5a6b9de9c5169bd50c6147f3da7
avachoi <alexbchoi@gmail.com>
2024-07-14 01:24:45 -0400

    created PUT and DELETE

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const morgan = require("morgan");
+const methodOverride = require("method-override");
 
 const usersData = require("./data/users.js");
 const postsData = require("./data/posts.js");
@@ -14,6 +15,7 @@ app.set("view engine", "pug");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 app.use((req, res, next) => {
 	const time = new Date();
 
@@ -49,19 +51,34 @@ app.get("/", (req, res) => {
 	};
 	res.render("users", usersList);
 });
-app.post("/", (req, res) => {});
+app.post("/", (req, res) => {
+	if (req.body.username && req.body.email) {
+		const newUser = {
+			id: usersData.length > 0 ? usersData[usersData.length - 1].id + 1 : 1,
+			username: req.body.username,
+			email: req.body.email,
+		};
+		usersData.push(newUser);
+		console.log("new user in the list", usersData[usersData.length - 1]);
+		res.redirect("/");
+	} else {
+		res.json({ error: "Insufficient Data for new user" });
+	}
+});
 
 app.get("/:id", (req, res) => {
-	console.log("id", req.params.id);
-	let user = usersData.find((user) => user.id == req.params.id);
-	console.log("user", user);
-	const usersPosts = {
-		user: user,
-		posts: postsData.filter((post) => post.userId == req.params.id),
-	};
+	let user = usersData.find((user) => user.id == parseInt(req.params.id));
+	if (user) {
+		const usersPosts = {
+			user: user,
+			posts: postsData.filter((post) => post.userId == req.params.id),
+		};
 
-	if (usersPosts) {
-		res.render("posts", usersPosts);
+		if (usersPosts) {
+			res.render("posts", usersPosts);
+		}
+	} else {
+		res.status(404).send("user not found");
 	}
 });
 app.post("/:id", (req, res) => {
@@ -83,6 +100,35 @@ app.post("/:id", (req, res) => {
 		res.redirect(`/${req.params.id}`);
 	} else {
 		res.json({ error: "Insufficient Data" });
+	}
+});
+
+app.get("/:id/:postId", (req, res) => {
+	const post = postsData.find((p) => p.id == req.params.postId);
+	if (post) {
+		res.render("post", { userId: post.userId, post: post });
+	} else {
+		res.json({ error: "Post not found" });
+	}
+});
+app.put("/:id/:postId", (req, res) => {
+	const postIndex = postsData.findIndex((p) => p.id == req.params.postId);
+	if (postIndex !== -1 && req.body.title && req.body.content) {
+		postsData[postIndex].title = req.body.title;
+		postsData[postIndex].content = req.body.content;
+		console.log("editied", postsData[postIndex]);
+		res.redirect(`/${req.params.id}`);
+	} else {
+		res.json({ error: "post not found or insufficient data" });
+	}
+});
+app.delete("/:id/:postId", (req, res) => {
+	const postIndex = postsData.findIndex((p) => p.id == req.params.postId);
+	if (postIndex) {
+		postsData.splice(postIndex, 1);
+		res.redirect(`/${req.params.id}`);
+	} else {
+		res.json({ error: "post to be deleted not found" });
 	}
 });
 
